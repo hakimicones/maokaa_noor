b780817b8403d43c09956641fea3dddbe8ca9b97
Nouas A/HAKIM <63954985+hakimicones@users.noreply.github.com>
2026-05-25 08:42:23 +0100

    ajout images

--- app/views/partials/navbar.php
+++ app/views/partials/navbar.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm sticky-top">
     <div class="container">
         <a class="navbar-brand fw-bold" href="<?php echo BASE_URL; ?>">
-            <span style="color: #667eea; font-size: 24px;">VEP</span>
+             <img src="<?php echo BASE_URL; ?>assets/img/logo.jpg" alt="Logo VEP" style="height:40px;">
         </a>
         
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
